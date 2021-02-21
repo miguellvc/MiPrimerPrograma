aec52f0983398f5a40ea6cac7bc35f3100ec9725
Miguel Perez <miguellrvc@gmail.com>
2021-02-21 19:21:57 -0300

    Mi primer commit

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<!-- hoja de estilo personalizada -->
+	<link rel="stylesheet" href="style.css">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Tp_1</title>
+</head>
+<body>
+<div class=wrap>
+
+	<form action="procesar.php?" method="post">
+		<h1>Datos del Usuario</h1>
+		<label>Nombre</label>
+		<input type="text" name='nombre'>
+		<label>Apellido</label>
+		<input type="text" name="apellido">
+		<input type="submit" name="Enviar">
+
+	</form>
+
+</div>
+</body>
+</html>
---
+++ procesar.php
@@ -0,0 +1,12 @@
+<?php
+ $nombre=$_POST['nombre'];
+ $apellido=$_POST['apellido']; 
+ if (isset($_POST['nombre']))
+
+	echo "El nombre es: ". $nombre; 
+	
+	echo "<br>El apellido es ". $apellido;
+ 
+ //$apellido=$_POST['apellido'];
+	
+?>
